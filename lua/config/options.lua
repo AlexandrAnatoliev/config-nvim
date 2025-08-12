76666a02321a5845cefc9d1169b7b73ac8355486
AlexandrAnatoliev <per-1986@list.ru>
2025-08-12 20:55:48 +0300

    fix: add new options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,7 @@
 -- Add any additional options here
 
 vim.opt.scrolloff = 8 -- прокручивать, когда до низа остается 8 строк
+vim.opt.wrap = false -- Отключаем перенос строк
+vim.opt.linebreak = false -- Отключаем "умный" перенос по словам
+vim.opt.sidescrolloff = 8 -- Прокручивать, когда до правого края остаётся 8 символов
+vim.opt.sidescroll = 1 -- Плавная горизонтальная прокрутка
